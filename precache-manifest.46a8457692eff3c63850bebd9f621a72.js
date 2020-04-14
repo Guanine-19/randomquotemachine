00611c62051a25063c809c8a2d2b0a1067421b3f
Guanine-19 <fudgeebarr12@gmail.com>
2020-04-14 21:32:11 +0800

    Updates

---
+++ precache-manifest.46a8457692eff3c63850bebd9f621a72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c2d834fdfba38cc1ded29bde9e60ca",
+    "url": "/randomquotemachine/index.html"
+  },
+  {
+    "revision": "df6624fe019a96aa1f82",
+    "url": "/randomquotemachine/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6208f727323aa6859dfe",
+    "url": "/randomquotemachine/static/css/main.6405a163.chunk.css"
+  },
+  {
+    "revision": "df6624fe019a96aa1f82",
+    "url": "/randomquotemachine/static/js/2.76566ede.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/randomquotemachine/static/js/2.76566ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6208f727323aa6859dfe",
+    "url": "/randomquotemachine/static/js/main.b8583aaa.chunk.js"
+  },
+  {
+    "revision": "885c5edc802f56efd56c",
+    "url": "/randomquotemachine/static/js/runtime-main.519c92e7.js"
+  }
+]);
